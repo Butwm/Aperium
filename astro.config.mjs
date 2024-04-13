213c83a5f75fde7143d420c6da8c6a07fd1d3d70
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-13 11:56:09 +0200

    upate

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  site: "https://butwm.github.io",
-  base: "/Aperium",
+  site: "https://aperium.butwm.space/",
+  base: ".",
 });
