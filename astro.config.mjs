ef37b3774203371c2b34066443f3a64e5a5ee195
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-13 11:45:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
 import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   site: "https://aperium.butwm.space/",
   base: "/Aperium",
+  output: "server",
+  adapter: cloudflare()
 });
