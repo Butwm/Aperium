861af20d443afe92b236e089755491af6e47ff93
dan <105148379+uqnc@users.noreply.github.com>
2024-04-21 12:15:23 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 export default {
     pricingCardPremiumUrl: 'https://www.google.com',
     pricingCardLifetimeUrl: 'https://www.google.com',
-    instagramlinkurl: 'https://www.instagram.com/google/',
-    discordlinkurl: 'https://discord.gg/google',
-    mailto: 'mailto:someone@example.com',
+    instagramlinkurl: 'https://www.instagram.com/aperium/',
+    discordlinkurl: 'https://discord.aperium.sk',
+    mailto: 'info@aperium.sk',
     schedulemeetingurl: 'https://google.com/',
     starthere: 'https://google.com',
     onepercent: '#pricing',
