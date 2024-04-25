bd3949fc32188e4e8c3e6932aa3eb7aaf8010574
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-25 20:11:20 +0200

    feat(feature3 redirect)

--- src/config.js
+++ src/config.js
@@ -41,6 +41,7 @@ export default {
     CardBeginnerFeature1: '1:1 Mentoring',
     CardBeginnerFeature2: 'Networking',
     CardBeginnerFeature3: 'More info',
+    CardBeginnerFeature3Url: 'https://discord.google.com',
 
     // Aperium Advance
     CardAdvanceUrl: 'https://buy.stripe.com/7sI6ouaXmgFB2527sx',
@@ -55,7 +56,7 @@ export default {
     CardAdvanceFeature1: '1:1 Mentoring',
     CardAdvanceFeature2: 'Networking',
     CardAdvanceFeature3: 'More info',
-    
+    CardAdvanceFeature3Url: 'https://discord.google.com',
     
     // We are not as usual
     NotAsUsual: 'We are not as usual',
