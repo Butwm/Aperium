00ed34c34f699e6a631410b74ac1b788f32ec3c4
dan <105148379+uqnc@users.noreply.github.com>
2024-05-29 21:56:32 +0200

    Update 29.5

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ export default {
 
     // Hero
     HeroHeading: 'Your Future. Our Focus.',
-    onepercenturl: '#pricing',
-    StartHereText: 'Start Your Plan Today',
+    onepercenturl: 'https://calendly.com/aperium',
+    StartHereText: 'Start Growing',
     starthere: '#pricing',
 
     // Our Program
@@ -28,35 +28,20 @@ export default {
     Pricing: 'Pricing',
     PricingHeading: 'How much it costs?',
 
-    // Aperium Beginner
-    CardBeginnerUrl: 'https://buy.stripe.com/dR6bIO0iI895eRO5kq',
-    CardBeginnerButton: 'Join Now',
-    CardBeginnerPriceVAT: '+ VAT / monthly',
-    CardBeginnerText: 'Beginner Level',
-    CardBeginnerDesc: 'Cancel at anytime.',
-    CardBeginnerPrice: '$20',
-    CardBeginnerPriceCents: '.00',
-
-    //Aperium Beginner Features
-    CardBeginnerFeature1: 'Class Mentoring',
-    CardBeginnerFeature2: 'Basic algorithm',
-    CardBeginnerFeature3: 'More info',
-    CardBeginnerFeature3Url: 'https://discord.aperium.sk',
-
     // Aperium Advance
-    CardAdvanceUrl: 'https://buy.stripe.com/7sI6ouaXmgFB2527sx',
-    CardAdvancePriceVAT: '+ VAT / monthly',
-    CardAdvanceButton: 'Join Now',
-    CardAdvanceText: 'Advance Level',
+    CardAdvanceUrl: 'https://calendly.com/aperium',
+    CardAdvancePriceVAT: '+ VAT / Monthly',
+    CardAdvanceButton: 'Start Now',
+    CardAdvanceText: 'Individual Pricing',
     CardAdvanceDesc: 'Cancel at anytime.',
-    CardAdvancePrice: '$50',
-    CardAdvancePriceCents: '.00',
+    CardAdvancePrice: '$?.',
+    CardAdvancePriceCents: '??',
 
     //Aperium Advance Features
-    CardAdvanceFeature1: '1:1 Mentoring',
-    CardAdvanceFeature2: 'Advance algorithm',
+    CardAdvanceFeature1: 'Paid Promotion',
+    CardAdvanceFeature2: 'Sfc Campaign',
     CardAdvanceFeature3: 'More info',
-    CardAdvanceFeature3Url: 'https://discord.aperium.sk',
+    CardAdvanceFeature3Url: 'https://calendly.com/aperium',
     
     // We are not as usual
     NotAsUsual: 'We are not as usual',
